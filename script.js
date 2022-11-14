540bc34bcebb822e6dc31f6b25a1db0a73e08b97
Sophia Ellis <sophiaelllisdesign@gmail.com>
2022-11-14 04:29:23 +0000

    added formula to js

--- script.js
+++ script.js
@@ -2,5 +2,30 @@
 // total for profit/lost
 // total changes
 // average changes
-// greatest increase 
-// greatest decrease 
+// greatest increase
+// greatest decrease
+
+let total = 0;
+let averageChange = 0;
+
+// Finds average
+for (let i = 0; i < finances.length; i++) {
+  total += finances[i][1];
+
+  for (let k = 1; k < finances.length; k++) {
+    averageChange += (finances[k][1] - finances[i][1]) / (finances[i][1] * 100);
+  }
+}
+
+// Console Logs
+console.log("Financial Analysis\n-------------------");
+console.log(`Total Months: ${finances.length}`);
+console.log(`Total: $${total}`);
+console.log(`Average Changes: $${Math.round(total / finances.length)}`);
+console.log(
+  `Greatest Increase: ${finances[finances.length - 1][0]} ($${
+    finances[finances.length - 1][1]
+  })`
+);
+console.log(`Greatest Decrease: ${finances[0][0]} ($ ${finances[0][1]})`);
+console.log("Financial Analysis\n-------------------");
