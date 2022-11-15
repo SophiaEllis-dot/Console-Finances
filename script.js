c548ac8d25a4baa53d79436591163c7d15616839
Sophia Ellis <sophiaelllisdesign@gmail.com>
2022-11-15 21:36:12 +0000

    changed average loop

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-// List of vars
 // total for profit/lost
 // total changes
 // average changes
 // greatest increase
 // greatest decrease
 
+
+
 let total = 0;
 let averageChange = 0;
+var array = finances;
+var i = 0,
+  sum = 0,
+  average = 0;
 
-// Finds average
-for (let i = 0; i < finances.length; i++) {
-  total += finances[i][1];
 
-  for (let k = 1; k < finances.length; k++) {
-    averageChange += (finances[k][1] - finances[i][1]) / (finances[i][1] * 100);
-  }
+  
+// loop for finding sum
+
+for (i = 0; i < finances.length; i++) {
+  sum = sum + finances[i][1];
 }
+// finds average
+average = sum / finances.length;
+average_round = average.toFixed(2);
 
-// Console Logs
+
+
+// Console logs
 console.log("Financial Analysis\n-------------------");
 console.log(`Total Months: ${finances.length}`);
-console.log(`Total: $${total}`);
-console.log(`Average Changes: $${Math.round(total / finances.length)}`);
+console.log("Total $" + sum);
+console.log("Average Changes $" + average_round);
 console.log(
   `Greatest Increase: ${finances[finances.length - 1][0]} ($${
     finances[finances.length - 1][1]
   })`
 );
 console.log(`Greatest Decrease: ${finances[0][0]} ($ ${finances[0][1]})`);
+
 console.log("Financial Analysis\n-------------------");
